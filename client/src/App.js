cb5c0470521c7b7161adf0e0d694195dac551fb7
alejo4373 <alejandrofranco@pursuit.org>
2021-01-29 17:44:30 -0500

    Add initial routing with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Switch, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Link to="/">Home</Link>{" "}
+        <Link to="/community">Community</Link>{" "}
+        <Link to="/register">Register</Link>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <h1>Home</h1>
+        </Route>
+        <Route path="/community">
+          <h1>community</h1>
+        </Route>
+        <Route path="/register">
+          <h1>Register</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
